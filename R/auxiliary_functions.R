e5770b04d68c18f0a80ee8c6c5ecb326a9ab1e25
Patrick Hoscheit <patrick.hoscheit@inra.fr>
2019-03-29 12:08:58 +0100

    Initial commit

---
+++ R/auxiliary_functions.R
@@ -0,0 +1,122 @@
+# Computes the total rate of coalescence for n lineages under the Beta(2-alpha,alpha)-coalescent
+
+TotalRate <- function(n,alpha) 
+{ 
+  if(n<2) return(0)
+  Comb <- sapply(2:n,function(x) lchoose(n,x))
+  Lambda <- sapply(2:n,function(x) lbeta(x-alpha,n-x+alpha))
+  Lambda <- Lambda-lbeta(2-alpha,alpha)
+  sum(sapply(Comb+Lambda,exp))
+}
+
+# Extension of the coalescent.intervals function in ape. Takes a phylogeny (class phylo) as input and returns a coalescentIntervals object,
+# which lists the interval lengths, in backwards time, along with the number of lineages during each of those intervals. Contrary to the ape
+# function, this can deal with multifurcations, as well as serially-sampled (non-ultrametric) trees.
+
+coalescent.intervals.multi <- function(x) 
+{
+  if (class(x) != "phylo") stop("object \"x\" is not of class \"phylo\"")
+  
+  # ordered branching times
+  t <- sort(branching.times.samp(x))
+  
+  t <- signif(t,digits=10) # Required to eliminate numerical instabilities
+  Dist <- unique(t) # We group all identical times together
+  lt <- length(Dist) # Total number of intervals
+  
+  # interval widths
+  w <- numeric(lt)
+  w[1] <- Dist[1]
+  for (i in 2:lt) w[i] <- Dist[i] - Dist[i - 1]
+  
+  # Logs if a coalescence happens or not
+  Coal <- logical(lt)
+  
+  # Logs the number of lineages involved in coalescences (breaks down in the case of simultaneous coalescences)
+  NumInvolved <- integer(lt)
+  
+  l<-numeric(lt)
+  l[1] <- 0 # Number of lineages at present time
+  for(i in 2:lt){
+    Nodes <- as.integer(names(t[t==Dist[i-1]])) # Identifies the nodes (leaves or internal) with timestamp Dist[i-1]
+    
+    CoalNodes <- Nodes[Nodes>length(x$tip.label)] # Finds all internal nodes, corresponding to a coalescence event
+    SampNodes <- Nodes[Nodes<=length(x$tip.label)] # Finds all leaves, corresponding to a sampling event
+    Involved <- 0 # Will record the total number of lineages coalescing at that time
+    
+    if(length(CoalNodes) >0){
+      # if(length(CoalNodes)>1) print("Simultaneous Coalescences detected! Results meaningless!")
+      Coal[i-1] <- TRUE
+      for(j in 1:length(CoalNodes)){
+        Edges <- x$edge[x$edge[,1]==CoalNodes[j],] # Finds how many lineages coalesce at node CoalNodes[j]
+        Involved <- Involved + length(Edges[,1]) # Updates the count of coalescing lineages
+        NumInvolved[i-1] <- length(Edges[,1])
+      }
+    }
+    l[i]<-l[i-1]-Involved+length(CoalNodes)+length(SampNodes)
+  }
+  NumInvolved[lt] <- l[lt]
+  Coal[lt] <- TRUE
+  obj <- list(
+    lineages=l,
+    interval.length=w,
+    interval.count=lt,
+    total.depth =sum(w),
+    coalescences = Coal,
+    NumInvolved=NumInvolved)
+  class(obj) <- "coalescentIntervals"
+  return(obj)
+}
+
+# Extension of the branching.times function in ape. Takes a phylogeny (class phylo) and returns a vector with all event times in backwards time
+# including the coalescences and the sampling events.
+
+branching.times.samp <- function(phy)
+{
+  if (!inherits(phy, "phylo"))
+    stop('object "phy" is not of class "phylo"')
+  DistToRoot <- dist.nodes(phy)[length(phy$tip.label)+1,] # length(phy$tip.label)+1 is the label of the root, so we get all distances to the root
+  TotalDepth <- max(DistToRoot)
+  TotalDepth - DistToRoot # We want time backwards from the last sampling event
+}
+
+# Transforms a binary phylogeny into a multifurcating one, collapsing all branches shorter than tol
+
+di2multi.cons <- function(phylo, tol = 1e-8)
+{
+  if (is.null(phylo$edge.length)) stop("the tree has no branch length")
+  ## We select only the internal branches which are
+  ## significantly small:
+  ind <- which(phylo$edge.length < tol & phylo$edge[, 2] > length(phylo$tip.label))
+  n <- length(ind)
+  if (!n) return(phylo)
+  ## recursive function to `propagate' node #'s in case
+  ## there is a series of consecutive edges to remove
+  foo <- function(ancestor, des2del,length) {
+    wh <- which(phylo$edge[, 1] == des2del)
+    for (k in wh) {
+      if (phylo$edge[k, 2] %in% node2del) foo(ancestor, phylo$edge[k, 2],length+phylo$edge.length[k])
+      else{
+        phylo$edge[k, 1] <<- ancestor
+        phylo$edge.length[k] <<- phylo$edge.length[k]+length
+      } 
+    }
+  }
+  node2del <- phylo$edge[ind, 2]
+  anc <- phylo$edge[ind, 1]
+  long <- phylo$edge.length[ind]
+  for (i in 1:n) {
+    if (anc[i] %in% node2del) next
+    foo(anc[i], node2del[i],long[i])
+  }
+  phylo$edge <- phylo$edge[-ind, ]
+  phylo$edge.length <- phylo$edge.length[-ind]
+  phylo$Nnode <- phylo$Nnode - n
+  ## Now we renumber the nodes that need to be:
+  sel <- phylo$edge > min(node2del)
+  for (i in which(sel))
+    phylo$edge[i] <- phylo$edge[i] - sum(node2del < phylo$edge[i])
+  if (!is.null(phylo$node.label))
+    phylo$node.label <- phylo$node.label[-(node2del - length(phylo$tip.label))]
+  phylo
+}
---
+++ R/skyline_estimation.R
@@ -0,0 +1,95 @@
+require(ape)
+require(optimx)
+
+
+# Main estimation function : takes as input a coalescentIntervals object, as well as the alpha parameter of the Beta coalescent, and the grouping parameter
+# epsilon, and returns a skyline object, along with its log-likelihood
+
+Skyline.multi.coalescentIntervals <- function(Inter,alpha,epsilon=0.0){
+  N <- Inter$interval.count # Total number of intervals in the phylogeny
+  Factors <- sapply(Inter$lineages,function(x) TotalRate(x,alpha)) # Total rate of coalescence on each interval
+  Estimate <- numeric(N) # Logs the skyline estimate
+  Coal <- Inter$coalescences # Records if the intervals end with a coalescence or not (regardless of samplings)
+  Involved <- Inter$NumInvolved
+  
+  NumSampling <- 0
+  CurrEstimate <- 0
+  for(i in 1:N){
+    if(Coal[i]){
+      CurrEstimate <- CurrEstimate + Factors[i]*Inter$interval.length[i]
+      for(j in (i-NumSampling):i){
+        Estimate[j] <- CurrEstimate
+      }
+      NumSampling <- 0
+      CurrEstimate <- 0
+    }
+    else{
+      CurrEstimate <- CurrEstimate + Factors[i]*Inter$interval.length[i]
+      NumSampling <- NumSampling + 1
+    }
+  }
+  
+  NInter <- N
+  
+  if(epsilon > 0.0){
+    CollInter <- collapsed.intervals(Inter,epsilon)
+    NColl <- CollInter$collapsed.interval.count
+    EstimateColl <- numeric(NColl)
+    for(i in 1:NColl){
+      EstimateColl[i] <- mean(Estimate[CollInter$collapsed.interval==i])
+    }
+    for(i in 1:N){
+      Estimate[i] <- EstimateColl[CollInter$collapsed.interval[i]]
+    }
+    NInter <- NColl
+  }
+  
+  Comb <- numeric(N)
+  Lambdas <- numeric(N)
+  Contrib <- numeric(N)
+  for(i in 1:N){
+    if(Coal[i]){
+      # Comb[i] <- lchoose(Inter$lineages[i],Involved[i])
+      Lambdas[i] <- lbeta(Involved[i]-alpha,Inter$lineages[i]-Involved[i]+alpha) - lbeta(2-alpha,alpha)
+      Contrib[i] <- Comb[i]+Lambdas[i]-log(Estimate[i])-Factors[i]*(Inter$interval.length[i])/Estimate[i]
+    }
+    else{
+      Contrib[i] <- -Factors[i]*(Inter$interval.length[i])/Estimate[i]
+    }
+  }
+  obj <- list(
+    time=cumsum(Inter$interval.length),
+    interval.length=Inter$interval.length,
+    population.size = Estimate,
+    parameter.count=NInter,
+    epsilon=epsilon,
+    logL = sum(Contrib)  
+  )
+  class(obj)<- "skyline"
+  return(obj)
+}
+
+Skyline.multi.phylo <- function(phylo,alpha,epsilon=0.0){
+  Inter <- coalescent.intervals.multi(phylo)
+  Skyline.multi.ci(Inter,alpha,epsilon)
+}
+
+
+
+# Performs a maximum-likelihood estimation of the alpha parameter, with the multifurcating skyline
+# as demographic function.
+# epsilon is the grouping parameter, as defined originally in (Strimmer, Pybus 2001)
+
+BetaCoal.maxlik <- function(phylo,epsilon=0.0) {
+  Inter <- coalescent.intervals.multi(phylo)
+  optimx(par=1.5, fn=function(x) -Skyline.multi.coalescentIntervals(Inter,x,epsilon)$logL,lower=0.001,upper=1.999, method="bobyqa")
+}
+
+# Computes and plots the likelihood landscape of a given phylogeny as a function of alpha
+
+FitnessLandscape <- function(phylo,epsilon=0.0){
+  Inter <- coalescent.intervals.multi(x = phylo)
+  Lik <- sapply(seq(from=1,to=1.99,length.out = 100),function(x) Skyline.multi.coalescentIntervals(Inter,alpha=x,epsilon)$logL)
+  plot(seq(from=1,to=1.99,length.out = 100),Lik)
+  return(Lik)
+}
